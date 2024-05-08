1b2ae0e718dc9431e8078bf3e8878c074877f344
JaekSooley <117260365+JaekSooley@users.noreply.github.com>
2024-05-08 13:43:34 +1000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,8 +3,10 @@ using System;
 using System.IO;
 
 
+/*
 string dir0;
 string dir1;
+*/
 
 
 MainMenu();
@@ -15,183 +17,229 @@ void MainMenu()
     bool active = true;
     while (active)
     {
-        dir0 = "";
-        dir1 = "";
-
         UI.Header("Main Menu");
         UI.Write("Compare the contents of two folders");
+        UI.Option("[L]OAD", "Enter path of folders to compare.");
+        UI.Option("[S]ETTINGS");
         UI.Write("");
-        UI.Write("Enter path of first directory...");
 
-        string tempDir0 = Input.GetDirectory();
+        string input = Input.GetString();
 
-        if (tempDir0 != "")
+        switch (input.ToUpper())
         {
-            dir0 = tempDir0;
-
-            UI.Header("Compare to...");
-            UI.Write("Enter path of second directory...");
-            string tempDir1 = Input.GetDirectory();
-
-            if (tempDir1 != "")
-            {
-                dir1 = tempDir1;
-
-                CompareContents();
-            }
+            case "L":
+            case "LOAD":
+                CompareMenu();
+                break;
+            case "S":
+            case "SETTINGS":
+                break;
+            default:
+                break;
         }
     }
 }
 
 
-void CompareContents()
+void CompareMenu()
 {
-    string[] files0 = Directory.GetFiles(dir0);
-    string[] files1 = Directory.GetFiles(dir1);
+    string dir0;
+    string dir1;
 
-    Dictionary<string, FileEntry> results = new();
-
-    foreach (string file in files0)
+    bool active = true;
+    while (active)
     {
-        string path = file;
-        string name = Path.GetFileName(path);
+        UI.Header("Compare From");
+        UI.Write("Enter path of first directory...");
 
-        FileEntry entry = new();
+        string input = Input.GetDirectory();
 
-        entry.path = path;
-        entry.name = name;
-        entry.count = 1;
-
-        results[name] = entry;
-    }
-    
-    foreach (string file in files1)
-    {
-        string path = file;
-        string name = Path.GetFileName(path);
-
-        FileEntry entry = new();
-
-        entry.path = path;
-        entry.name = name;
-
-        if (results.ContainsKey(name))
+        if (Directory.Exists(input))
         {
-            results[name].count++;
+            dir0 = input;
+
+            UI.Header("Compare to...");
+            UI.Write("Enter path of second directory...");
+            string input1 = Input.GetDirectory();
+
+            if (Directory.Exists(input1))
+            {
+                dir1 = input1;
+
+                string[] files0 = Directory.GetFiles(dir0);
+                string[] files1 = Directory.GetFiles(dir1);
+
+                Dictionary<string, FileEntry> results = new();
+
+                foreach (string file in files0)
+                {
+                    string path = file;
+                    string name = Path.GetFileName(path);
+
+                    FileEntry entry = new();
+
+                    entry.path = path;
+                    entry.name = name;
+                    entry.count = 1;
+
+                    results[name] = entry;
+                }
+
+                foreach (string file in files1)
+                {
+                    string path = file;
+                    string name = Path.GetFileName(path);
+
+                    FileEntry entry = new();
+
+                    entry.path = path;
+                    entry.name = name;
+
+                    if (results.ContainsKey(name))
+                    {
+                        results[name].count++;
+                    }
+                    else
+                    {
+                        results[name] = entry;
+                    }
+                }
+
+                // Duplicate files
+                List<string> duplicateFiles = new();
+
+                foreach (string key in results.Keys)
+                {
+                    if (results[key].count > 1)
+                    {
+                        duplicateFiles.Add(key);
+                    }
+                }
+
+                // Unique files
+                List<string> uniqueFiles0 = new();
+                List<string> uniqueFiles1 = new();
+
+                foreach (string key in results.Keys)
+                {
+                    if (results[key].count <= 1)
+                    {
+                        string dir = Path.GetDirectoryName(results[key].path);
+
+                        if (dir != null)
+                        {
+                            if (dir == dir0)
+                            {
+                                uniqueFiles0.Add(key);
+                            }
+                            else if (dir == dir1)
+                            {
+                                uniqueFiles1.Add(key);
+                            }
+                            else
+                            {
+                                UI.Error("You done fucked up the code something fierce.");
+                            }
+
+                        }
+                    }
+                }
+
+
+                UI.Header("Done");
+                UI.Write("Show duplicates?");
+                UI.Option("Y", "Yes");
+                UI.Option("N", "No");
+
+                bool showDuplicates = true;
+
+                bool loop = true;
+                while (loop)
+                {
+                    string showDuplicatesInput = Input.GetString("Y");
+
+                    switch (showDuplicatesInput.ToUpper())
+                    {
+                        case "Y":
+                            showDuplicates = true;
+                            loop = false;
+                            break;
+                        case "N":
+                            showDuplicates = false;
+                            loop = false;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                // DUPLICATE FILES
+
+                UI.Header("Compare Contents");
+
+                UI.Write("");
+                UI.Write($"{duplicateFiles.Count} duplicate files found");
+
+
+                if (showDuplicates)
+                {
+                    UI.Write("");
+
+                    foreach (string key in duplicateFiles)
+                    {
+                        UI.Write($"\t\"{key}\"");
+                    }
+                }
+
+                UI.Write("");
+                UI.Write("");
+
+                UI.Write("---------------------------------------------------");
+
+
+                // UNIQUE FILES
+
+                UI.Write("");
+                UI.Write("");
+                UI.Write($"{uniqueFiles0.Count} unique files in \"{dir0}\"");
+                UI.Write("");
+                foreach (string key in uniqueFiles0)
+                {
+                    UI.Write($"\t\"{key}\"");
+                }
+                UI.Write("");
+                UI.Write("");
+
+                UI.Write("---------------------------------------------------");
+
+                UI.Write("");
+                UI.Write("");
+                UI.Write($"{uniqueFiles1.Count} unique files in \"{dir1}\"");
+                UI.Write("");
+                foreach (string key in uniqueFiles1)
+                {
+                    UI.Write($"\t\"{key}\"");
+                }
+
+                UI.Write("");
+                UI.Write("");
+
+                // TOTALS
+
+                int uniqueCount = uniqueFiles0.Count + uniqueFiles1.Count;
+
+                UI.Write($"{uniqueCount} unique files, {duplicateFiles.Count} duplicates.");
+                UI.Write($"Total count: {uniqueCount + duplicateFiles.Count}");
+
+
+                UI.Pause();
+            }
         }
         else
         {
-            results[name] = entry;
+            active = false;
         }
     }
-
-    // Duplicate files
-    List<string> duplicateFiles = new();
-
-    foreach (string key in results.Keys)
-    {
-        if (results[key].count > 1)
-        {
-            duplicateFiles.Add(key);
-        }
-    }
-
-    // Unique files
-    List<string> uniqueFiles0 = new();
-    List<string> uniqueFiles1 = new();
-
-    foreach (string key in results.Keys )
-    {
-        if (results[key].count <= 1)
-        {
-            string dir = Path.GetDirectoryName(results[key].path);
-            if (dir == dir0)
-            {
-                uniqueFiles0.Add(key);
-            }
-            else if (dir == dir1)
-            {
-                uniqueFiles1.Add(key);
-            }
-            else
-            {
-                UI.Error("You done fucked up the code something fierce.");
-            }
-        }
-    }
-
-
-    UI.Header("Done");
-    UI.Write("Show duplicates?");
-    UI.Option("true", "Yes");
-    UI.Option("false", "No");
-
-    bool showDuplicates = Input.GetBoolean(false);
-
-    // DUPLICATE FILES
-
-    UI.Header("Compare Contents");
-
-    UI.Write("");
-    UI.Write($"{duplicateFiles.Count} duplicate files found");
-
-
-    if (showDuplicates)
-    {
-        UI.Write("");
-
-        foreach (string key in duplicateFiles)
-        {
-            UI.Write($"\t\"{key}\"");
-        }
-    }
-
-    UI.Write("");
-    UI.Write("");
-
-    UI.Write("---------------------------------------------------");
-
-
-    // UNIQUE FILES
-
-    UI.Write("");
-    UI.Write("");
-    UI.Write($"{uniqueFiles0.Count} unique files in \"{dir0}\"");
-    UI.Write("");
-    foreach (string key in uniqueFiles0)
-    {
-        UI.Write($"\t\"{key}\"");
-    }
-    UI.Write("");
-    UI.Write("");
-
-    UI.Write("---------------------------------------------------");
-
-    UI.Write("");
-    UI.Write("");
-    UI.Write($"{uniqueFiles1.Count} unique files in \"{dir1}\"");
-    UI.Write("");
-    foreach (string key in uniqueFiles1)
-    {
-        UI.Write($"\t\"{key}\"");
-    }
-
-    UI.Write("");
-    UI.Write("");
-
-    // TOTALS
-
-    int uniqueCount = uniqueFiles0.Count + uniqueFiles1.Count;
-
-    UI.Write($"{uniqueCount} unique files, {duplicateFiles.Count} duplicates.");
-    UI.Write($"Total count: {uniqueCount + duplicateFiles.Count}");
-
-
-    UI.Pause();
-
-    duplicateFiles.Clear();
-    uniqueFiles0.Clear();
-    uniqueFiles1.Clear();
 }
 
 
